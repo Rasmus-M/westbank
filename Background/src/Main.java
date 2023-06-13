1e132e075dd6a4b043b6d0638a6c890b19a49b6c
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2023-06-13 05:43:44 +0200

    Duel patterns; backgrounds

---
+++ Background/src/Main.java
@@ -0,0 +1,39 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+
+    static int[] colors = {7, 7, 7, 7, 7, 7, 7, 7, 12, 12, 12};
+
+    public static void main(String[] args) throws IOException {
+
+        byte[] buffer = new byte[0x20000];
+
+        FileInputStream fis = new FileInputStream("westbank8.bin");
+        fis.read(buffer);
+        fis.close();
+
+        for (int bank = 5; bank < 12; bank++) {
+            for (int ch = 0; ch < 7; ch++) {
+                int base = bank * 0x2000 + 0x40 + ch * 0x0420 + 0x210;
+                for (int row = 0; row < 11; row++) {
+                    for (int i = 0; i < 0x30; i++) {
+                        int addr = base + row * 0x30 + i;
+                        int b = buffer[addr] & 0xff;
+                        int h = (b & 0xf0) >> 4;
+                        int l = (b & 0x0f);
+                        if (h == 7) h = colors[row];
+                        if (l == 7) l = colors[row];
+                        b = (h << 4) | l;
+                        buffer[addr] = (byte) b;
+                    }
+                }
+            }
+        }
+
+        FileOutputStream fos = new FileOutputStream("westbankx8.bin");
+        fos.write(buffer);
+        fos.close();
+    }
+}
