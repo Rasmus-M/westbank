466069f628d042bade54bbce2bd203a4155bacc8
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2023-07-01 19:12:06 +0200

    Dusk and night

--- Background/src/Main.java
+++ Background/src/Main.java
@@ -4,7 +4,14 @@ import java.io.IOException;
 
 public class Main {
 
-    static int[] colors = {7, 7, 7, 7, 7, 7, 7, 7, 11, 11, 11};
+
+    static final int N_CHARACTER_BANKS = 7;
+    static final int startBankDay = 4;
+    static final int[] dayColors = {7, 7, 7, 7, 7, 7, 7, 7, 11, 11, 11};
+    static final int startBankDusk = 18;
+    static final int[] duskColors = {4, 4, 4, 4, 4, 4, 4, 4, 10, 10, 10};
+    static final int startBankNight = 25;
+    static final int[] nightColors = {0, 0, 0, 0, 0, 0, 0, 0, 14, 14, 14};
 
     public static void main(String[] args) throws IOException {
 
@@ -14,7 +21,20 @@ public class Main {
         fis.read(buffer);
         fis.close();
 
-        for (int bank = 4; bank < 11; bank++) {
+        System.arraycopy(buffer, startBankDay * 0x2000, buffer, startBankDusk * 0x2000, N_CHARACTER_BANKS * 0x2000);
+        System.arraycopy(buffer, startBankDay * 0x2000, buffer, startBankNight * 0x2000, N_CHARACTER_BANKS * 0x2000);
+
+        applyColorsToBanks(startBankDay, dayColors, buffer);
+        applyColorsToBanks(startBankDusk, duskColors, buffer);
+        applyColorsToBanks(startBankNight, nightColors, buffer);
+
+        FileOutputStream fos = new FileOutputStream("westbank8.bin");
+        fos.write(buffer);
+        fos.close();
+    }
+
+    private static void applyColorsToBanks(int startBank, int[] colors, byte[] buffer) {
+        for (int bank = startBank; bank < startBank + N_CHARACTER_BANKS; bank++) {
             for (int ch = 0; ch < 7; ch++) {
                 int base = bank * 0x2000 + 0x40 + ch * 0x0420 + 0x210;
                 for (int row = 0; row < 11; row++) {
@@ -31,9 +51,5 @@ public class Main {
                 }
             }
         }
-
-        FileOutputStream fos = new FileOutputStream("westbank8.bin");
-        fos.write(buffer);
-        fos.close();
     }
 }
