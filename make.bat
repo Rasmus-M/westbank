cfd341159da44da6ed9fe35afbb28fdc82db2b1f
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2023-07-07 20:44:53 +0300

    V2

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 xas99.py src/main-memory.a99 -i -q -R -L westbank.lst -E symbols.txt -o bin/main
 xas99.py src/rom-banks.a99 -B -q -R -o bin/banks.bin
 
-java -jar tools/ea5tocart.jar bin\main "WEST BANK"
+java -jar tools/ea5tocart.jar bin\main "WEST BANK V2"
 
 copy /b bin\main8.bin ^
     + bin\banks.bin ^
